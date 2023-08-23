50c2e214f4550006cf3073081e40e96de231a5aa
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-08-23 13:00:16 -0400

    Update to latest Prettier tailwind CSS version and apply mass formatting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    extend: {
-      margin: {
-        "space-x": "var(--space-x)",
-      },
-      padding: {
-        "space-x": "var(--space-x)",
-      },
-      transitionDuration: {
-        "350": "350ms",
-        "400": "400ms",
-      }
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			margin: {
+				"space-x": "var(--space-x)",
+			},
+			padding: {
+				"space-x": "var(--space-x)",
+			},
+			transitionDuration: {
+				350: "350ms",
+				400: "400ms",
+			},
+		},
+	},
+	plugins: [],
 };
