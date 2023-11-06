bea5774ce2c91dbc880eab92dfb0642f6482e635
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-11-06 17:50:21 -0500

    Remark plugin for custom heading id

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import { remarkHeadingId } from "remark-custom-heading-id";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -9,4 +9,7 @@ export default defineConfig({
 	site: "https://bhuynh.dev",
 	integrations: [mdx(), sitemap(), tailwind({ applyBaseStyles: false })],
 	scopedStyleStrategy: "class",
+	markdown: {
+		remarkPlugins: [remarkHeadingId],
+	},
 });
